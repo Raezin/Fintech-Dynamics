70a8c94e5ded78fcea60ca4f9a7bce8a457af39c
Abaan Sohail <abaansohail131@gmail.com>
2026-09-03 15:59:03 +0500

    modified new component

--- src/components/new-component.js
+++ src/components/new-component.js
@@ -3,7 +3,9 @@ import React from 'react'
 export default function NewComponent() {
   return (
     <div>
-      This is the new component.
+      <p>This is a new component.</p>
+      <Button>Press Here</Button>
+      <a href="#">Click me</a>
     </div>
   )
 }
